9a9119148ae4498ffb46a5c0c0d6781ef7afc5ba
Danil Ryazanov <danilryazanov@mail.ru>
2016-04-06 19:49:35 +0300

    Signed-off-by: Данил Рязанов <Данил Рязанов>

--- main.cpp
+++ main.cpp
@@ -5,9 +5,13 @@ using namespace std;
 
 int main()
 {
+	int a;
     cout << "Hello Sashka!" << endl;
-	сout << "asdsad"<<endl;
-	cout << "dsada"<endl;
+	cout << "asdsad"<<endl;
+	if(2>1)
+	{
+		cin>>a;
+	}
     getch();
     return 0;
 }
